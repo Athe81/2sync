02061c5403c384ef7ae13a2f1cd098abbac4b251
BigBart <t.matt81+github@gmail.com>
2014-03-14 23:43:28 +0100

    Some bigger code reorganisations

--- 2sync.py
+++ 2sync.py
@@ -6,6 +6,7 @@ import shutil
 import logging
 import argparse
 import hashlib
+from collections import namedtuple
 
 def log_and_raise(msg, e=None):
 	"""
@@ -31,6 +32,22 @@ class ExitError(Exception):
 	def __str__(self):
 		return repr(self.value)
 
+def get_hash(file):
+	"""
+	Returns the SHA1 hash of a file
+	"""
+	_config_hash = hashlib.sha1()
+	
+	try:
+		f = open(file, 'rb')
+		_config_hash.update(f.read())
+	except FileNotFoundError as e:
+		log_and_raise("File: '" + file + "' does not exist", e)
+	except PermissionError as e:
+		log_and_raise("No permission to file: '" + file + "'", e)
+	f.close()
+	return _config_hash.hexdigest()
+
 def test_string(parsed_filters, string):
 	"""
 	Test a string with the given, parsed filters
@@ -43,6 +60,7 @@ def test_string(parsed_filters, string):
 		sum_filters = len(filters)
 		for pos in range(sum_filters):
 			if sum_filters == 1 and pre == 0 and post == 0 and string == filters[0]:
+				logging.debug("'" + string + "' matched filter: '" + str(pre) + ", " + str(post) + ", " + str(filters) + "'")
 				return True
 			if pre == 0 and pos == 0 and filters[pos] != string[:len(filters[pos])]:
 				stat = 0
@@ -51,6 +69,7 @@ def test_string(parsed_filters, string):
 				stat = 0
 				break
 			if sum_filters == 1 and (pre == 0 or post == 0):
+				logging.debug("'" + string + "' matched filter: '" + str(pre) + ", " + str(post) + ", " + str(filters) + "'")
 				return True
 			str_pos = string[str_pos:].find(filters[pos])
 			if str_pos == -1:
@@ -58,32 +77,128 @@ def test_string(parsed_filters, string):
 				break
 			str_pos += len(filters[pos])
 		if stat == 1:
+			logging.debug("'" + string + "' matched filter: '" + str(pre) + ", " + str(post) + ", " + str(filters) + "'")
 			return True
+		logging.debug("'" + string + "' doesn't matched filter: '" + str(pre) + ", " + str(post) + ", " + str(filters) + "'")
 	return False
 
-class sync():
+class BasicData(object):
+	_filetype = namedtuple('_filetype', 'stat, moddate')
+	_foldertype = namedtuple('_foldertype', 'stat')
+
+	def __init__(self):
+		self._files = dict()
+		self._folders = dict()
+		
+	def add_file(self, file, stat, moddate):
+		self._files[file] = self._filetype(stat, moddate)
+		
+	def add_folder(self, folder, stat):
+		self._folders[folder] = self._foldertype(stat)
+		
+	def get_file(self, file):
+		return self._files[file]
+		
+	def get_folder(self, folder):
+		return self._folders[folder]
+	
+	@property
+	def files(self):
+		# return [(k, v) for k, v in self._files.items()]
+		return self._files
+	
+	@property
+	def folders(self):
+		# return [(k, v) for k, v in self._folders.items()]
+		return self._folders
+
+class PersistenceData(BasicData):
+	# TODO: Convert configname to data filename
+	def __init__(self):
+		super().__init__()
+		self._load_data()
+		
+	def _load_data(self):
+		"""
+		Loads the saved information about synchronised files and folders
+		"""
+		try:
+			f = open('saved_data', 'rb')
+		except FileNotFoundError:
+			logging.warn("Could not load data from file: '" + 'files' + "'. File not found")
+		except PermissionError as e:
+			log_and_raise("Could not load data from file: '" + 'folders' + "'. No Permission", e)
+		else:
+			for (key, values) in pickle.load(f):
+				if len(values) == 2:
+					self.add_file(key, values[0], values[1])
+				else:
+					self.add_folder(key, values[0])
+			f.close()
+	
+	def _save_data(self):
+		"""
+		Save the informations about synchronised files and folders
+		"""
+		try:
+			with open('saved_data', 'wb') as f: 
+				pickle.dump([(k, [v.stat, v.moddate]) for k, v in self._files.items()] + [(k, [v.stat]) for k, v in self._folders.items()], f)
+		except Exception as e:
+			log_and_raise("Could not save data to: '" + 'folders' + "'", e)
+	
+	def add_file(self, file, stat, moddate):
+		super().add_file(file, stat, moddate)
+		self._save_data()
+		
+	def add_folder(self, file, stat):
+		super().add_folder(file, stat)
+		self._save_data()
+
+class FSData(BasicData):
+	def _stat(self, file):
+		"""
+		Return the permissions for a file or folder
+		"""
+		try:
+			return oct(os.lstat(file).st_mode)[-3:]
+		except PermissionError:
+			log_and_raise("Could not read stat from: '" + file + "'", e)
+	
+	def _moddate(self, file):
+		"""
+		Return the last modification date from a file or folder
+		"""
+		try:
+			return os.lstat(file).st_mtime
+		except Exception as e:
+			log_and_raise("Could not read moddate from: '" + file + "'", e)
+			
+	def __init__(self, path):
+		self._path = path
+		super().__init__()
+	
+	def add_file(self, file):
+		path = self._path + "/" + file
+		super().add_file(file, self._stat(path), self._moddate(path))
+		
+	def add_folder(self, folder):
+		path = self._path + "/" + folder
+		super().add_folder(folder, self._stat(path))
+		
+	@property
+	def path(self):
+		return self._path
+
+class sync(object):
 	"""
 	Main class for the syncronisation
 	"""
-	class data():
-		"""
-		A class for a root
-		
-		Holds the root specific data's
-		"""
-		def __init__(self, path):
-			self.path = path
-			self.files = dict()
-			self.folders = dict()
-			self.changed_files = set()
-			self.changed_folders = set()
 	def __init__(self, config):
-		roots = config.roots
-		self.roots = []
+		self.sync_data = PersistenceData()
+		self.root0 = FSData(config.roots[0])
+		self.root1 = FSData(config.roots[1])
 		self.file_conflicts = set()
 		self.folder_conflicts = set()
-		self.roots.append(self.data(roots[0]))
-		self.roots.append(self.data(roots[1]))
 		self.files = []
 		self.folders = []
 		self.copy = []
@@ -97,6 +212,7 @@ class sync():
 			config._save_config_hash()
 			
 		self._find_changes(config)
+		# self._find_changes(config)
 	
 	def _load_data(self):
 		"""
@@ -155,51 +271,50 @@ class sync():
 			return os.lstat(file).st_mtime
 		except Exception as e:
 			log_and_raise("Could not read moddate from: '" + file + "'", e)
-	
+				
 	def _find_files(self, config):
 		"""
 		Read files/folders from root's
 		
 		If file/folder match the filters it will be saved with last modfication date and permissions
 		"""
-		def _test_and_add(folder, files):
+		def _test_and_add(sub_path, files):
 			"""
 			Test files and folder with ignore filters and returns the files and folder who sould synchronised
 			"""
-			sub_path = path[len_root_path:] + "/"
 			# Add files and folders if 'ignore not path' match to folder
 			if test_string(config.ignore_not_path, sub_path) == True:
-				root.folders[sub_path] = self._stat(root.path + sub_path)
+				root.add_folder(sub_path)
 				for file in files:
-					file = sub_path + file
-					root.files[file] = (self._stat(root.path + file), self._moddate(root.path + file))
+					root.add_file(sub_path + file)
 				return
 			
-			# Continue if 'ignore path' match does not match file
 			if test_string(config.ignore_path, sub_path) == True:
 				return
-			# Add file if 'ignore path' not match
-			root.folders[sub_path] = self._stat(root.path + sub_path)
+			
+			# Add while 'ignore path' doesn't match
+			root.add_folder(sub_path)
 			
 			for file in files:
 				# Add file if 'ignore not file' match the filename
 				if test_string(config.ignore_not_file, file) == True:
-					file = sub_path + file
-					root.files[file] = (self._stat(root.path + file), self._moddate(root.path + file))
-					return
+					root.add_file(sub_path + file)
+					continue
+				
 				# Ignore file if the 'ignore file' match the filename
 				if test_string(config.ignore_file, file) == True:
-					return
-				# Add file if 'ignore not file' and 'ignore file' not matched
-				file = sub_path + file
-				root.files[file] = (self._stat(root.path + file), self._moddate(root.path + file))
-
-		for x in range(len(self.roots)):
-			root = self.roots[x]
-			len_root_path = len(root.path)
-			for path, _, files in os.walk(root.path, followlinks=False):
-				_test_and_add(path, files)
+					continue
 				
+				# Add file if 'ignore not file' and 'ignore file' not matched
+				root.add_file(sub_path + file)
+
+		for root in (self.root0, self.root1):
+			len_root_path = len(root.path)
+			logging.debug("Test files for root: '" + root.path + "'")
+			for path, _, files in os.walk(root.path, followlinks=False):
+				sub_path = path[len_root_path:] + "/"
+				_test_and_add(sub_path, files)
+		
 	def _find_changes(self, config):
 		"""
 		Prepare the changed files for the synchronisation
@@ -211,15 +326,24 @@ class sync():
 		Save the necessary actions  
 		"""
 		self._find_files(config)
-		for x in range(len(self.roots)):
-			root = self.roots[x]
-			root.changed_files = set(self.files) ^ set([(x, y) for x, y in root.files.items()])
-			root.changed_folders = set(self.folders) ^ set([(x, y) for x, y in root.folders.items()])
+		
+		for root in (self.root0, self.root1):
+			root.changed_files = self.sync_data.files.items() ^ root.files.items()
+			root.changed_folders = self.sync_data.folders.items() ^ root.folders.items()
 			root.changed_files = set([f for (f, *_) in root.changed_files])
 			root.changed_folders = set([f for (f, *_) in root.changed_folders])
-			
-		self.file_conflicts = set(self.roots[0].changed_files) & set(self.roots[1].changed_files)
-		self.folder_conflicts = set(self.roots[0].changed_folders) & set(self.roots[1].changed_folders)
+		
+		self.file_conflicts = self.root0.changed_files & self.root1.changed_files
+		self.folder_conflicts = self.root0.changed_folders & self.root1.changed_folders
+		
+		for file in self.root0.files:
+			self.sync_data.add_file(file, stat=self.root0.get_file(file).stat, moddate=self.root0.get_file(file).moddate)
+		for folder in self.root0.folders:
+			self.sync_data.add_folder(folder, stat=self.root0.get_folder(folder).stat)
+		
+		# for sub_path in self.file_conflicts:
+			# TODO: WORK 
+			# self._inspect_content(sub_path)
 		
 		# Solve conflicts
 		if len(self.file_conflicts) != 0 or len(self.folder_conflicts) != 0:
@@ -227,15 +351,14 @@ class sync():
 
 		_file_conflicts = set()
 		for sub_path in self.file_conflicts:
-			for x in range(len(self.roots)):
-				if sub_path in self.roots[x].files.keys():
-					stat, moddate = self.roots[x].files[sub_path]
-					print("File: " + self.roots[x].path + sub_path + ", Stat: " + stat + ", Moddate:" + str(moddate))
+			for root in (self.root0, self.root1):
+				if sub_path in root.files.keys():
+					print("File: " + root.path + sub_path + ", Stat: " + root.files[sub_path].stat + ", Moddate:" + str(root.files[sub_path].moddate))
 				else:
-					print("File: " + self.roots[x].path + sub_path + " deleted")
+					print("File: " + root.path + sub_path + " deleted")
 			while True:
 				try:
-					action = int(input("0: ignore, 1: " + self.roots[0].path + sub_path + " is master, 2: " + self.roots[1].path + sub_path + " is master "))
+					action = int(input("0: ignore, 1: " + self.root0.path + sub_path + " is master, 2: " + self.root1.path + sub_path + " is master "))
 				except ValueError:
 					action = -1
 				
@@ -243,10 +366,10 @@ class sync():
 					_file_conflicts.add(sub_path)
 					break
 				elif action == 1:
-					self.roots[1].changed_files.remove(sub_path)
+					self.root1.changed_files.remove(sub_path)
 					break
 				elif action == 2:
-					self.roots[0].changed_files.remove(sub_path)
+					self.root0.changed_files.remove(sub_path)
 					break
 				
 				print("Wrong input. Please insert a correct input")
@@ -255,19 +378,14 @@ class sync():
 		
 		new_folder_conflict = set()
 		for sub_path in self.folder_conflicts:
-			for x in range(len(self.roots)):
-				if sub_path in self.roots[x].folders.keys():
-					stat = self.roots[x].folders[sub_path]
-					print("Folder: " + self.roots[x].path + sub_path + ", Stat: " + stat)
+			for root in (self.root0, self.root1):
+				if sub_path in root.folders.keys():
+					print("Folder: " + root.path + sub_path + ", Stat: " + root.folders[sub_path].stat)
 				else:
-					print("Folder: " + self.roots[x].path + sub_path + " deleted")
+					print("Folder: " + root.path + sub_path + " deleted")
 			while True:
 				try:
-					action = int(input("0: ignore, 1: " + self.roots[0].path + sub_path + " is master, 2: " + self.roots[1].path + sub_path + " is master "))
-				except KeyboardInterrupt:
-					print()
-					logging.critical("Exit programm while KeyboardInterrupt (ctrl + c)")
-					exit()
+					action = int(input("0: ignore, 1: " + self.root0.path + sub_path + " is master, 2: " + self.root1.path + sub_path + " is master "))
 				except ValueError:
 					action = -1
 					
@@ -275,18 +393,17 @@ class sync():
 					new_folder_conflict.add(sub_path)
 					break
 				elif action == 1:
-					self.roots[1].changed_folders.remove(sub_path)
+					self.root1.changed_folders.remove(sub_path)
 					break
 				elif action == 2:
-					self.roots[0].changed_folders.remove(sub_path)
+					self.root0.changed_folders.remove(sub_path)
 					break
 				
 				print("Wrong input. Please insert a correct input")
 				
 			self.folder_conflicts = new_folder_conflict
 				
-		for x in range(len(self.roots)):
-			root = self.roots[x]
+		for root in (self.root0, self.root1):
 			def analyse_action(sub_path, src_root, dst_root, file_folder, conflicts):
 				def mark_for_copy(sub_path, src_root, dst_root, file_folder):
 					self.copy.append((sub_path, src_root, dst_root, file_folder))
@@ -300,12 +417,16 @@ class sync():
 					mark_for_copy(sub_path, src_root, dst_root, file_folder)
 				else:
 					mark_for_remove(sub_path, dst_root, file_folder)
-					
+			if root == self.root0:
+				other_root = self.root1
+			else:
+				other_root = self.root0
+				
 			for filename in root.changed_files:
-				analyse_action(filename, root.path, self.roots[1-x].path, 'file', self.file_conflicts)
+				analyse_action(filename, root.path, other_root.path, 'file', self.file_conflicts)
 				
 			for filename in root.changed_folders:
-				analyse_action(filename, root.path, self.roots[1-x].path, 'folder', self.folder_conflicts)
+				analyse_action(filename, root.path, other_root.path, 'folder', self.folder_conflicts)
 	
 	def do_action(self):
 		"""
@@ -385,6 +506,7 @@ class config(object):
 		"""
 		Create sha1 hash for config. Compare it with the existing sha1 hash for config. Save the new hash.
 		"""
+		logging.info("Check if config-file has changed")
 		_config_hash = hashlib.sha1()
 		
 		try:
@@ -412,7 +534,9 @@ class config(object):
 			f.close()
 		
 		if self._hexdigest == saved_hash:
+			logging.info("config-file has not changed")
 			return False
+		logging.info("config-file has changed")
 		return True
 	
 	def _save_config_hash(self):
@@ -531,20 +655,28 @@ class config(object):
 
 parser = argparse.ArgumentParser(description='2-way syncronisation for folders')
 parser.add_argument('config', help='name of the configuration file')
+parser.add_argument('-d', '--debug', help='use this option for debuging (write debug messages to logfile)', action='store_true', )
 args = parser.parse_args()
 
 # Config logging
-# Logging to file
-logging.basicConfig(level=logging.INFO, filename='2sync.log', filemode='a', format='%(levelname)s: %(asctime)s - 2sync - %(message)s')
+# Set loglevel für logfile
+if args.debug == True:
+	log_level = logging.DEBUG
+else:
+	log_level = logging.INFO
+
+# Logging to file	
+logging.basicConfig(level=log_level, filename='2sync.log', filemode='a', format='%(levelname)s: %(asctime)s - 2sync - %(message)s')
 # define a Handler for sys.stderr and add it
 console = logging.StreamHandler()
 console.setLevel(logging.WARNING)
 logging.getLogger('').addHandler(console)
 
+logging.info("Start program")
+
 try:
 	config = config(args.config)
 	sync = sync(config)
-#	sync.find_changes(config)
 	sync.do_action()
 except ExitError:
 	pass
